4a3964be75de1b4b4c8d92b022a4aefff152e117
Aaron Phan <ap3924@rit.edu>
2025-06-30 19:22:13 -0400

    Prototype of the Debris class

--- FloodBuds/Debris.cs
+++ FloodBuds/Debris.cs
@@ -6,5 +6,60 @@ namespace FloodBuds
 {
     internal class Debris
     {
+
+        private Texture2D sprite;
+
+        /// <summary>
+        /// The hitbox of the debris. Dependent on which type of debris it is.
+        /// </summary>
+        private Rectangle hitbox;
+        public Rectangle Hitbox { get { return hitbox; } set { Hitbox = value; } }
+
+
+        /// <summary>
+        /// The types of debris to spawn
+        /// </summary>
+        private enum DebrisType
+        {
+            DriftWood,
+            Tree,
+            Car
+        }
+
+        private DebrisType debrisType;
+
+        public Debris(Texture2D sprite)
+        {
+
+            this.sprite = sprite;
+            hitbox = new Rectangle();
+        }
+
+        public void Move()
+        {
+            switch (debrisType)
+            {
+                case DebrisType.DriftWood:
+                    hitbox.X += 5;
+                    break;
+
+                case DebrisType.Tree:
+                    hitbox.X += 10;
+                    break;
+
+                case DebrisType.Car:
+                    hitbox.X += 1;
+                    break;
+            }
+            if(hitbox.X > 1920)
+            {
+                hitbox.X = -200;
+            }
+        }
+
+        public void Draw(SpriteBatch sb)
+        { 
+            sb.Draw(sprite, hitbox, Color.White);
+        }
     }
 }
