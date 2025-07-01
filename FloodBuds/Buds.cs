3e39d48d6d9e86c33651b9b1a79b3e908e6a0aa0
Aaron Phan <ap3924@rit.edu>
2025-06-30 21:37:47 -0400

    Start to Buds class

--- FloodBuds/Buds.cs
+++ FloodBuds/Buds.cs
@@ -1,10 +1,99 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace FloodBuds
 {
     internal class Buds
     {
+        private Random rng = new Random();
+        private Texture2D sprite;
+        private Rectangle hitbox;
+        public Rectangle Hitbox { get { return hitbox; } set { Hitbox = value; } }
+
+        private enum Direction
+        {
+            Left,
+            Right,
+            Top,
+            Bottom
+        }
+
+        private Direction direction;
+
+        public Buds(Texture2D sprite)
+        {
+            this.sprite = sprite;
+            hitbox = new Rectangle(0, 0, 65, 65);
+        }
+
+        public void Move()
+        {
+            hitbox.X += 5;
+            hitbox.Y += 5;
+        }
+
+        public bool CheckDespawn()
+        {
+            switch (direction)
+            {
+                case Direction.Left:
+                    return hitbox.X > 1920;
+
+                case Direction.Right:
+                    return hitbox.X < 0 - hitbox.Width;
+
+                case Direction.Top:
+                    return hitbox.Y > 1080;
+
+                case Direction.Bottom:
+                    return hitbox.Y < 0 - hitbox.Height;
+            }
+            return false;
+        }
+
+        public void GenerateValues()
+        {
+            switch (direction)
+            {
+                // Left side
+                case Direction.Left:
+
+                    hitbox.X = 0 - hitbox.Width;
+                    hitbox.Y = rng.Next(1, 1080 - hitbox.Height);
+
+                    break;
+
+                // Right side
+                case Direction.Right:
+
+                    hitbox.X = 1920;
+                    hitbox.Y = rng.Next(1, 1080 - hitbox.Height);
+
+                    break;
+
+                // Top
+                case Direction.Top:
+
+                    hitbox.X = rng.Next(1, 1920 - hitbox.Width);
+                    hitbox.Y = 0 - hitbox.Height;
+
+                    break;
+
+                // Bottom
+                case Direction.Bottom:
+
+                    hitbox.X = rng.Next(1, 1920 - hitbox.Width);
+                    hitbox.Y = 1080;
+
+                    break;
+            }
+        }
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(sprite, hitbox, Color.White);
+        }
     }
 }
